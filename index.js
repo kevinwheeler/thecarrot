c1e3fc8fd627790a62b3720918e4fc98be71fa31
TimMac13 <timmac13@TimMac13s-MacBook-Pro.local>
2016-01-24 21:13:53 -0600

    Removed 'cool' ascii npm module and route. Also added 404 handler.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
 var env = process.env.NODE_ENV;
 
 // from http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect
@@ -27,8 +26,8 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-	  response.send(cool());
+app.use(function(req, res, next) {
+	  res.status(404).send('Sorry cant find that!');
 });
 
 app.listen(app.get('port'), function() {
