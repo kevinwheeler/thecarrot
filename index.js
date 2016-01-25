0408cbf5e3bae83d76411ad307751cdd620cd9c4
TimMac13 <timmac13@TimMac13s-MacBook-Pro.local>
2016-01-24 20:18:51 -0600

    Added force https when in production

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var env = process.env.NODE_ENV;
+
+// from http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect
+var forceSsl = function (req, res, next) {
+   if (req.headers['x-forwarded-proto'] !== 'https') {
+     return res.redirect(301, ['https://', req.get('Host'), req.url].join(''));
+   }
+   return next();
+};
+
+if (env === 'production') {
+    app.use(forceSsl);
+}
 
 app.set('port', (process.env.PORT || 5000));
 
