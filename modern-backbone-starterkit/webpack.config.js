fb3f3f1e85ecf25800c6b9dccd81c3203289f8fd
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2016-04-24 13:37:36 -0500

    moved sharedaliases and others into one object, sharedExports

--- modern-backbone-starterkit/webpack.config.js
+++ modern-backbone-starterkit/webpack.config.js
@@ -1,53 +1,55 @@
 var _ = require('lodash');
+var path = require('path');
 var entry = './src/app/main.js',
   output = {
     path: __dirname,
     filename: 'main.js'
   };
 
-// Loader patterns that should be used both in development and production. Factored out here.
-var sharedLoaders = [
-  { test: /node_modules.bootstrap.*\.js?$/, loader: 'imports?jQuery=jquery' },
-  { test: /node_modules.slick-carousel\/slick\/slick.min.js?$/ , loader: 'imports?jQuery=jquery,$=jquery' },
-  { test: /.(gif|png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' },//https://github.com/webpack/css-loader/issues/38
-  { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
-  { test: /\.hbs$/, loader: 'handlebars-loader' },
-  { test: /\.css$/, loader: 'style-loader!css-loader' }
-];
+// exports that are used both in production and in development
+var sharedExports = {
+  module : {
+      loaders : [
+        { test: /bootstrap.dist.*\.js?$/, loader: 'imports?jQuery=jquery' },
+        { test: /node_modules.slick-carousel\/slick\/slick.min.js?$/ , loader: 'imports?jQuery=jquery,$=jquery' },
+        { test: /.(gif|png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' },//https://github.com/webpack/css-loader/issues/38
+        { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.hbs$/, loader: 'handlebars-loader' },
+        { test: /\.css$/, loader: 'style-loader!css-loader' }
+     ]
+  },
 
-var sharedAliases = {
-  bootstrapCSS: 'bootstrap/dist/css/bootstrap.min.css',
-  bootstrapTheme: 'bootstrap/dist/css/bootstrap-theme.min.css',
-  slick: 'slick-carousel/slick/slick.min.js',
-  slickCSS: 'slick-carousel/slick/slick.css',
-  slickTheme: 'slick-carousel/slick/slick-theme.css'
-};
+  resolve: {
+    alias: {
+      bootstrapCSS: 'bootstrap/dist/css/bootstrap.min.css',
+      bootstrapTheme: 'bootstrap/dist/css/bootstrap-theme.min.css',
+      slick: 'slick-carousel/slick/slick.min.js',
+      slickCSS: 'slick-carousel/slick/slick.css',
+      slickTheme: 'slick-carousel/slick/slick-theme.css'
+    },
+    fallback: path.join(__dirname, "node_modules")
+  }
+}
 
 module.exports.development = {
     debug : true,
     devtool : 'eval',
     entry: entry,
     output: output,
-    module : {
-        loaders : [].concat(sharedLoaders)
-    },
     resolve: {
-      alias: _.merge({
-        'bootstrap$': "bootstrap/dist/js/bootstrap.js",
-      }, sharedAliases)
+      alias:  {'bootstrap$': "bootstrap/dist/js/bootstrap.js"},
     }
 };
+_.merge(module.exports.development, sharedExports);
 
 module.exports.production = {
     debug: false,
     entry: entry,
     output: output,
-    module : {
-        loaders : [].concat(sharedLoaders)
-    },
     resolve: {
-      alias: _.merge({
+      alias: {
         bootstrap: "bootstrap/dist/js/bootstrap.min.js",
-      }, sharedAliases)
+      }
     }
 };
+_.merge(module.exports.production, sharedExports);
