61a3fe94ddd1807b4e07231ae83ff36801720c02
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2016-08-15 19:34:38 -0500

    Added some copywriting to the article page

--- modern-backbone-starterkit/src/app/views/articleView.js
+++ modern-backbone-starterkit/src/app/views/articleView.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import Backbone from 'backbone';
 
 import template from 'TEMPLATESDIR/articleTemplate.hbs';
+import 'STYLESDIR/stylus/article.css';
 import 'UTILSDIR/facebooksdk';
 
 //export default Marionette.ItemView.extend({
@@ -22,6 +23,7 @@ export default Backbone.View.extend({
       article: window.kmw.article,
       //http://stackoverflow.com/questions/5817505/is-there-any-method-to-get-url-without-query-string-in-java-script
       articleURL: [location.protocol, '//', location.host, location.pathname].join(''),
+      citationURL: "http://www.chicagotribune.com/bluesky/technology/ct-share-this-link-without-reading-it-ap-bsi-20160618-story.html",
       imageURL: window.kmw.article.imageURL
     }));
     this.attachSubViews();
