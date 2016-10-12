5814f74023f6f90352de5ca9b8da13e00357a81c
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2016-10-12 00:25:44 -0500

    temp fb partial.

--- modern-backbone-starterkit/src/app/utils/facebooksdk.js
+++ modern-backbone-starterkit/src/app/utils/facebooksdk.js
@@ -1,35 +1,8 @@
-let facebookInitialized = false;
-const elementsToParseOnLoad= [];
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : window.kmw.fbAppId,
-    xfbml      : false,
-    version    : 'v2.7'
-  });
-
-  facebookInitialized = true;
-  for (let i=0; i < elementsToParseOnLoad.length; i++) {
-    const pair = elementsToParseOnLoad[i];
-    const el = pair[0];
-    const cb = pair[1];
-    window.FB.XFBML.parse(el, cb);
-  }
-};
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 const parseFbElement =  function(el, cb) {
-  if (facebookInitialized) {
+  if (window.kmw.facebookInitialized) {
     window.FB.XFBML.parse(el, cb);
   } else {
-    elementsToParseOnLoad.push([el, cb]);
+    window.kmw.fbElementsToParseOnLoad.push([el, cb]);
   }
 }
 
